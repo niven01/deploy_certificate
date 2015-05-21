67da11a0505b30e4501caf82a000bb937caebe7a
Nielsen Pierce <nielsen.pierce@rackspace.co.uk>
2015-05-21 10:50:54 +0000

    Add test recipe

---
+++ test/fixtures/cookbooks/deploy_certificate_tests/metadata.rb
@@ -0,0 +1,9 @@
+# Encoding: utf-8
+name 'deploy_certificate_tests'
+maintainer 'Rackspace'
+maintainer_email 'rackspace-cookbooks@rackspace.com'
+license 'APACHE 2.0'
+description 'Certifcate deployment resource'
+version '1.0.0'
+
+depends 'deploy_certificate'
---
+++ test/fixtures/cookbooks/deploy_certificate_tests/recipes/deploy_cert.rb
@@ -0,0 +1,11 @@
+# Encoding: utf-8
+#
+# Cookbook Name:: deploy_certificate_tests
+# Recipe:: deploy_cert
+#
+# Copyright 2012-2015, Rackspace
+
+deploy_certificate 'cert_test' do
+  cert 'cert 4324324343535435435435'
+  key 'key 834985430985309843905'
+end
